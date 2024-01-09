1f097dee3cc9b0c8064b68e963664c7241af87d7
Aurora Sanderson <AuroraTuring@gmail.com>
2024-01-09 16:39:32 -0700

    Fix formatting and add method to remove $ and convert to integer

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,13 +1,10 @@
 class Visitor
-  attr_reader :name,
-              :height,
-              :spending_money,
-              :preferences
+  attr_reader :name, :height, :spending_money, :preferences
 
   def initialize(name, height, spending_money)
     @name = name
     @height = height
-    @spending_money = spending_money.to_i
+    @spending_money = spending_money.sub("$", "").to_i
     @preferences = []
   end
 
