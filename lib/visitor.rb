c7ea4343d90e4dd31e7fa7cc48c93d4c6b4bfa8e
Aurora Sanderson <AuroraTuring@gmail.com>
2024-01-09 10:19:50 -0700

    Add supporting methods.

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,14 +1,23 @@
 class Visitor 
 
-  attr_reader :name
-              :height 
-              :spending_money
+  attr_reader :name,
+              :height,
+              :spending_money,
+              :preferences
 
-  def initialize()
+  def initialize(name, height, spending_money)
     @name = name 
     @height = height 
-    @spending_money = 
+    @spending_money = spending_money.to_i
     @preferences = []
+  end
+
+  def add_preference(new_preference)
+    @preferences << new_preference
+  end
+
+  def tall_enough?(required_height)
+    height >= required_height
   end
 
 end
