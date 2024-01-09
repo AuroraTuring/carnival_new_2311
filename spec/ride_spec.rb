54a1f057d27a3df83284e790e7eb0ecaa3bab55f
Aurora Sanderson <AuroraTuring@gmail.com>
2024-01-09 10:53:33 -0700

    Add attribute tests and fill in initialize

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -1,1 +1,34 @@
-require 'spec_helper'
+require 'spec_helper' 
+require './lib/visitor' 
+require './lib/ride' 
+
+RSpec.describe Ride do
+  before 'each' do 
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+  end
+
+  it 'exists' do 
+    expect(@ride1).to be_a(Ride)
+  end
+
+  it 'has attributes' do
+    expect(@ride1.name).to eq("Carousel")
+    expect(@ride1.min_height).to eq(24)
+    expect(@ride1.admission_fee).to eq(1)
+    expect(@ride1.excitement).to eq(:gentle)
+    expect(@ride1.total_revenue).to eq(0)
+
+  end
+
+  # describe 'ttt' do 
+  #   it 'ttt' do
+      
+  #   end
+
+  # end
+
+end
+
+
