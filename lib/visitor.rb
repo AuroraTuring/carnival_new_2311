ea03c18746c8db17fc8d4e89524a09c5b4e48eed
Aurora Sanderson <AuroraTuring@gmail.com>
2024-01-09 09:26:32 -0700

    Add Visitor class, attr_reader, and initialize.

---
+++ lib/visitor.rb
@@ -0,0 +1,14 @@
+class Visitor 
+
+  attr_reader :name
+              :height 
+              :spending_money
+
+  def initialize()
+    @name = name 
+    @height = height 
+    @spending_money = 
+    @preferences = []
+  end
+
+end
